381c65612f9cf432e91b189ab0e66622789b6404
NiveenALSmadi <niveensmadi33@gmail.com>
2021-06-13 14:17:28 +0300

    done

--- game_of_greed/game_of_greed.py
+++ game_of_greed/game_of_greed.py
@@ -1,4 +1,38 @@
+import random
+
 class GameLogic:
-    pass
+
+
+   @staticmethod
+   def calculate_score():
+       pass
+
+
+
+
+
+
+
+   @staticmethod
+   def roll_dice(number):
+        result=[]
+        for i in range (number):
+           result.append(random.randint(1,6))
+        return  tuple(result)
+
+
+
+
+
+
+
+
+
 class Banker:
     pass
+
+
+
+if __name__ == "__main__":
+    dice= GameLogic()
+    print(dice.roll_dice(6))
