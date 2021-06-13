b469332ef2fe1e77064ddc5a42536c19f22fde8f
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-06-13 16:41:17 +0300

    Finish the Banker class

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -8,7 +8,7 @@ class Banker:
         self.shelved += num
    
     def bank(self):
-        self.balance = 0
+        # self.balance = 0
         self.balance += self.shelved
         self.shelved = 0
         return self.balance
