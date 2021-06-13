212178ba34330c3e41d0e95596293c8d1fb390bc
doaa-1996 <doaaobeidat1996@gmail.com>
2021-06-13 14:53:27 +0300

    add bank class

--- game_of_greed/game_of_greed.py
+++ game_of_greed/game_of_greed.py
@@ -1,2 +1,20 @@
 class Banker:
-    pass
+
+    def __init__(self):
+        self.shelved = 0
+        self.balance= 0
+ 
+    def shelf(self,num):
+        self.shelved += num
+   
+    def bank(self):
+        self.balance = 0
+        self.balance += self.shelved
+        self.shelved = 0
+        return self.balance
+
+    def clear_shelf(self):
+        self.shelved = 0
+                    
+
+                    
